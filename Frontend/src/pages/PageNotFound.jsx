d0dfa01b808356b0aa7ec86a3bbb0ab310906348
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-10-20 13:03:37 +0300

    Add files via upload
    Added page not found

---
+++ Frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <>
+    <div id='notfound'>
+      <h1><i>404 PAGE NOT FOUND</i></h1>
+    </div>
+    </>
+  )
+}
+
+export default PageNotFound
