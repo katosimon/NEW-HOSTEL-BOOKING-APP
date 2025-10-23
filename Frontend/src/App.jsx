b7d1b6e15891d4f0ff93228f81e3324258486366
BUKOMEKO.W.HANNINGTON <bukomekohannington50@gmail.com>
2025-10-23 18:17:48 +0300

    Change route path for HomePage to '/intro'

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/intro" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/hostels" element={<Hostels />} />
