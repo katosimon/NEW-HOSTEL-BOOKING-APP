ca48975d58cbf413717a3c53ffdf446f18efbc09
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-10-24 16:49:45 +0300

    Add Toaster component for notifications

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary'
+import { Toaster } from 'react-hot-toast'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ErrorBoundary>
       <App />
+      <Toaster/>
     </ErrorBoundary>
   </StrictMode>,
 )
