3008ff421e8f698660ed7f19a4bb3e48ccbd28da
BUKOMEKO.W.HANNINGTON <bukomekohannington50@gmail.com>
2025-10-27 15:17:59 +0300

    Refactor routes and imports in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/Navbar';
 import Login from './pages/login';
-import Signup from './pages/Signup';
+import Signup  from './pages/Signup';
 import Hostels from './pages/Hostels';
 import HostelDetails from './pages/Hostelsdetails';
 import Booking from './pages/Booking';
 import Account from './pages/Account';
 import AdminDashboard from './pages/AdminDashboard';
-import ContactUs from './pages/Contact us';
+import ContactUs from './pages/Contactus';
 import ProtectedRoute from './components/ProtectedRoute';
 import AdminRoute from './components/AdminRoute';
 import HomePage from './pages/intro';
 import './styles.css';
+import Footer from './components/footer';
+import TermsOfServicePage from './pages/TermsOfServicePage';
+import FAQPage from './pages/FAQsPage';
+
 
 function App() {
   return (
@@ -21,16 +25,19 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/intro" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
           <Route path="/hostels" element={<Hostels />} />
           <Route path="/hostel/:id" element={<HostelDetails />} />
           <Route path="/contact" element={<ContactUs />} />
-          <Route path="/booking/:id" element={<ProtectedRoute><Booking /></ProtectedRoute>} />
+          <Route path="/booking/:id" element={<Booking />} />
           <Route path="/account" element={<ProtectedRoute><Account /></ProtectedRoute>} />
           <Route path="/admin" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+          <Route path="/terms" element={<TermsOfServicePage />} />
+          <Route path="/faqs" element={<FAQPage />} />
         </Routes>
+        <Footer />
       </Router>
     </AuthProvider>
   );
