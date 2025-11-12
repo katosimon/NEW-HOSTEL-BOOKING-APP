4a8484c891efc26b4f410f814a3a00d72fe3c918
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 21:41:11 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
-import  hostelRoute from '../Routes/hostelRoute.js'
-import bookingRoute from '../Routes/bookingRoute.js'
-import userRoute from '../Routes/userRoute.js'
+import  hostelRoute from './Routes/hostelRoute.js'
+import bookingRoute from './Routes/bookingRoute.js'
+import userRoute from './Routes/userRoute.js'
 import express from 'express';
-import { DbConnect } from './db.js';
+import { DbConnect } from './Routes/db.js';
 import cors from 'cors';
 export const app = express();
 app.use(cors());
@@ -17,3 +17,4 @@ const PORT = process.env.port || 8080;
 app.listen(PORT, () => {
   console.log(`example is on port ${PORT}`);
 });
+
