c812c2f6bf4ce1abf728a0a7ae4c3b356f105586
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 22:03:12 +0300

    Update hostelController.js

--- Backend/controllers/hostelController.js
+++ Backend/controllers/hostelController.js
@@ -1,5 +1,5 @@
 import Hostel from '../Models/Hostel.js';
-export  const getHostels = async (req, res) => {
+export const getHostels = async (req, res) => {
   try {
     const hostels = await Hostel.find();
     res.status(200).json(hostels);
@@ -11,14 +11,33 @@ export  const getHostels = async (req, res) => {
   }
 };
 
-export  const addHostel = async (req, res) => {
+export const getHostelById = async (req, res, next) => {
+  try {
+    const hostel = await Hostel.findById(req.params.id);
+
+    if (!hostel) {
+      return res.status(404).json({ message: 'Hostel not found' });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: hostel, 
+    });
+  } catch (error) {
+    if (error.kind === 'ObjectId') {
+      return res.status(404).json({ message: 'Hostel not found' });
+    }
+    next(error);
+  }
+};
+
+export const addHostel = async (req, res) => {
   try {
     const { name, location, type, price, shuttle, comment } = req.body;
-        const exists =await Hostel.findOne({name})
-        if(exists){
-          return res.status(400).json("Hostel already exists rename to save")
-        }
-
+    const exists = await Hostel.findOne({ name });
+    if (exists) {
+      return res.status(400).json('Hostel already exists rename to save');
+    }
 
     if (!name || !location || !price || !type || !shuttle) {
       return res.status(400).json({
@@ -26,7 +45,6 @@ export  const addHostel = async (req, res) => {
         message: 'Please provide all required fields',
       });
     }
-    
 
     const newHostel = await Hostel.create(req.body);
     res.status(201).json({
@@ -39,8 +57,7 @@ export  const addHostel = async (req, res) => {
     res.status(500).json({
       success: false,
       message: 'Server Error: Could not add hostel.',
-      reason: error
+      reason: error,
     });
   }
 };
-
