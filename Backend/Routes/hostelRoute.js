e136b26ec7c9a9d90ccb824cf71fc57fd201ad1c
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 21:50:04 +0300

    Update hostelRoute.js

--- Backend/Routes/hostelRoute.js
+++ Backend/Routes/hostelRoute.js
@@ -1,7 +1,11 @@
 import express from 'express';
-import { getHostels, addHostel } from '../controllers/hostelController.js';
+import {
+  getHostels,
+  addHostel,
+  getHostelById,
+} from '../controllers/hostelController.js';
 const router = express.Router();
 
-router.get('/:id', getHostels).post('/', addHostel);
+router.get('/', getHostels).post('/', addHostel).get('/:id', getHostelById);
 
 export default router;
