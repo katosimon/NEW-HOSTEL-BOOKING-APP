ba2304bc0bc1cfb5b22a3acb0f7a4b41915d17a0
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 22:46:03 +0300

    Update AdminRoute.jsx

--- Frontend/src/components/AdminRoute.jsx
+++ Frontend/src/components/AdminRoute.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContextContext';
+import { useAuth } from '../context/AuthContext';
 
 const AdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
@@ -12,3 +12,4 @@ const AdminRoute = ({ children }) => {
 };
 
 export default AdminRoute;
+
