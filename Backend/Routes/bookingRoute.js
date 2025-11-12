1f7f645210f9f9f558e1a1a32f996e8a20c554bb
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 21:45:16 +0300

    Fix import paths in bookingRoute.js

--- Backend/Routes/bookingRoute.js
+++ Backend/Routes/bookingRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addBooking,getBookings } from '../controllers/bookingController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { addBooking,getBookings } from './controllers/bookingController.js';
+import { protect } from './middleware/authMiddleware.js';
 const router =express.Router();
 
 router.get('/',getBookings).post('/', protect,addBooking);
+
 
 
 export default router;
