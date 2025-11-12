6bf67946dfa5a34d040422f3868adf08e3a79c73
Alvin-sserunkuuma <133165842+Alvin-sserunkuuma@users.noreply.github.com>
2025-11-12 21:51:19 +0300

    Update bookingRoute.js

--- Backend/Routes/bookingRoute.js
+++ Backend/Routes/bookingRoute.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { addBooking,getBookings } from './controllers/bookingController.js';
-import { protect } from './middleware/authMiddleware.js';
+import { addBooking,getBookings,deleteBooking } from '../controllers/bookingController.js';
+import { protect } from '../middleware/authMiddleware.js';
 const router =express.Router();
 
-router.get('/',getBookings).post('/', protect,addBooking);
-
+router.get('/',protect,getBookings).post('/', protect,addBooking).delete('/:id',protect,deleteBooking)
 
 
 export default router;
